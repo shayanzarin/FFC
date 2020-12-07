a3f64984cfb06df56637755132aaeb467ad39594
shayan <shayan.zarin.pars@gmail.com>
2020-12-08 01:05:14 +0330

    Test

---
+++ src/FFC.java
@@ -0,0 +1,5 @@
+public class FFC {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Hello {
+    public static void main(String[] args) {
+        List<Object> objectList;
+        List<String> stringList = null;
+
+        objectList = Collections.singletonList(stringList);
+
+    }
+}
